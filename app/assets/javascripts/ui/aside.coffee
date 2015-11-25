8acbdce220c1d242d3aa3d25032f6e2ac29f1c81
cassiuschen <chzsh1995@gmail.com>
2015-11-25 21:41:38 +0800

    add hash hahaha

--- app/assets/javascripts/ui/aside.coffee
+++ app/assets/javascripts/ui/aside.coffee
@@ -1,7 +1,24 @@
 define ["jquery"], ($) ->
 	init: ->
+		window.onhashchange = (->)
+		@parseURI()
+		@handleClick()
+	
+	parseURI: ->
+		if window.location.hash
+			$('aside.sidenav .select.active').removeClass 'active'
+			$('aside.contents section').removeClass 'show'
+			target = window.location.hash.substr(1)
+			$("aside.sidenav .select[data-target='#{target}']").addClass 'active'
+			$("aside.contents section##{target}").addClass 'show'
+		else
+			$("aside.sidenav .select:first-child").addClass 'active'
+			$("aside.contents section:first-child").addClass 'show'
+	handleClick: ->
 		$('aside.sidenav a.select').on 'click', (el) ->
 			$('aside.contents section').removeClass 'show'
 			$('aside.sidenav .select.active').removeClass 'active'
 			$(@).addClass 'active'
 			$("aside.contents section##{$(@).data 'target'}").addClass 'show'
+			window.location.hash = $(@).data 'target'
+			window.scrollTo(0,0)
--- public/assets/js/ui/aside.js
+++ public/assets/js/ui/aside.js
@@ -1,11 +1,31 @@
 define(["jquery"], function($) {
   return {
     init: function() {
+      window.onhashchange = (function() {});
+      this.parseURI();
+      return this.handleClick();
+    },
+    parseURI: function() {
+      var target;
+      if (window.location.hash) {
+        $('aside.sidenav .select.active').removeClass('active');
+        $('aside.contents section').removeClass('show');
+        target = window.location.hash.substr(1);
+        $("aside.sidenav .select[data-target='" + target + "']").addClass('active');
+        return $("aside.contents section#" + target).addClass('show');
+      } else {
+        $("aside.sidenav .select:first-child").addClass('active');
+        return $("aside.contents section:first-child").addClass('show');
+      }
+    },
+    handleClick: function() {
       return $('aside.sidenav a.select').on('click', function(el) {
         $('aside.contents section').removeClass('show');
         $('aside.sidenav .select.active').removeClass('active');
         $(this).addClass('active');
-        return $("aside.contents section#" + ($(this).data('target'))).addClass('show');
+        $("aside.contents section#" + ($(this).data('target'))).addClass('show');
+        window.location.hash = $(this).data('target');
+        return window.scrollTo(0, 0);
       });
     }
   };
